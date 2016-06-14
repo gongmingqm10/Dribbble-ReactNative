b2f5702fb042f02cd16ae85d612157d454ecea26
mingong <gongmingqm10@foxmail.com>
2016-06-14 15:50:17 +0100

    Setup the initial project

---
+++ DataRepository.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import {AsyncStorage} from 'react-native';
+
+var BASE_URL = "https://api.dribbble.com";
+var ACCESS_TOKEN = "34d7a0e44d9e6231eed4291f81e6390a9dd0baf24e9c9b0fea992fe9c24baa4e";
+
+module.exports = DataRepository;
+
+
